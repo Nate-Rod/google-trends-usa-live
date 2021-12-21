61505a858859aa34f6c001ce9eb789dc83639606
Nate-Rod <nate-rod@live.com>
2021-12-21 02:02:39 -0600

    Regex for API response text to isolate human-readable search query

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from trend_data import get_todays_top_hits, get_top_hit_for_state
 
 def create_app():
     tmp_app = Flask(__name__)
 
     @tmp_app.route('/')
     def hello_world():
-       return render_template('main.html')
+        top_hits = get_todays_top_hits()
+        for hit in top_hits:
+            print(hit)
+        get_top_hit_for_state()
+        return render_template('main.html')
     return tmp_app
 
 app = create_app()
