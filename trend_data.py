d4eb40366fdf46d2bbf597d47da987b95bb2269b
Nate-Rod <nate-rod@live.com>
2021-12-21 18:02:10 -0600

    Aggregate Google Trends data per search term per state

--- trend_data.py
+++ trend_data.py
@@ -10,22 +10,30 @@ pytrend = TrendReq(hl='en-US', tz=360, timeout=(10,25), retries=5, backoff_facto
 def get_todays_top_hits(region="US", cutoff=20):
     return pytrend.today_searches(pn=region).head(cutoff)
 
+def get_live_top_hits():
+    return pytrend.trending_searches(pn='united_states')
+
 def get_top_hit_for_state():
     top_hits = map(parse_top_hit, get_todays_top_hits())
-    # Query the last 30 days
+    print(get_live_top_hits())
+
     today_date = datetime.now().date()
-    start_date = today_date - timedelta(hours=1)
+    start_date = today_date - timedelta(days=30)
 
     # Create a string with the start and end dates
     timeframe_interval = "".join([str(start_date), " ", str(today_date)])
+
+    results_by_state_df = pd.DataFrame()
+
     for hit in top_hits:  # LIST HAS MORE THAN FIVE ELEMENTS, SO WE WILL USE A FOR LOOP
-        print(hit)
         pytrend.build_payload(kw_list=[hit], cat=0, timeframe=timeframe_interval, geo='US')
 
         # Interest by region
         data_reg = pytrend.interest_by_region(resolution='COUNTRY', inc_low_vol=True, inc_geo_code=False)
-        print(data_reg)
-    return
+        results_by_state_df = pd.concat(
+            [results_by_state_df, data_reg], join='outer', axis=1
+        )
+    return results_by_state_df
 
 def parse_top_hit(hit):
     # finding the search term based off of surrounding characters
