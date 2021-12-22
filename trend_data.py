ba828b1958f14336e487933fb56ecf2810b9793c
Nate-Rod <nate-rod@live.com>
2021-12-21 18:03:23 -0600

    Remove unused code

--- trend_data.py
+++ trend_data.py
@@ -10,12 +10,8 @@ pytrend = TrendReq(hl='en-US', tz=360, timeout=(10,25), retries=5, backoff_facto
 def get_todays_top_hits(region="US", cutoff=20):
     return pytrend.today_searches(pn=region).head(cutoff)
 
-def get_live_top_hits():
-    return pytrend.trending_searches(pn='united_states')
-
 def get_top_hit_for_state():
     top_hits = map(parse_top_hit, get_todays_top_hits())
-    print(get_live_top_hits())
 
     today_date = datetime.now().date()
     start_date = today_date - timedelta(days=30)
